5fa48961181e0cb898ea161716cdfafcfa0f8e66
Zied <26070035+zguesmi@users.noreply.github.com>
2026-08-06 15:22:18 +0200

    chore: un-ignore handle

--- sql/ignore_handles.sql
+++ sql/ignore_handles.sql
@@ -15,5 +15,4 @@ UPDATE handles SET ignored = TRUE
 WHERE handle_id IN (
   '0x0000066eee2301a9105e5da7d6be716294cbaf21bdfb1c2b8006300cbce6e6fa', -- Arbitrum sepolia
   '0x0000066eee230158b2b9696532102543ccb687971d8c6b8765174ccd912072d2', -- Arbitrum sepolia
-  '0x0000066eee2301b69660aba62301169484a6d10613a05a111c28fcf6beb96492'  -- Arbitrum sepolia
 );
